7938dd68c99afad31cdca5a10691649748f45f28
andreasc06 <andreassteinnes@gmail.com>
2025-02-20 20:27:31 +0100

    loads tetris rom to bus at path ../bin/tetris.gb

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod cpu;
 mod bus;
 mod instructions;
@@ -12,11 +11,15 @@ fn main() {
         println!("CPU Initialized")
     }
 
-    let rom = include_bytes!("../bin/dmg_boot.bin");
-    
+    let boot_rom = include_bytes!("../bin/dmg_boot.bin");
+    let game_rom = include_bytes!("../bin/tetris.gb");
     // Loads bootrom from 0x000-0x100
-    for (i, byte) in rom.iter().enumerate() {
-        cpu.bus.write(i as u16, *byte);
+    for (i, byte) in boot_rom.iter().enumerate() {
+        //cpu.bus.write(i as u16, *byte);
+        cpu.bus.boot_rom[i] = *byte;
+    } 
+    for (i, byte) in game_rom.iter().enumerate() {
+        cpu.bus.cartridge[i] = *byte;
     } 
 
     cpu.boot()
